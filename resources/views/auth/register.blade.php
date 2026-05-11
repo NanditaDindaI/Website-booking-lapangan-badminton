495342d9bd5640c2f9a2fa23216e3d8f6fe7024e
arradhin <106361735+arradhin@users.noreply.github.com>
2026-05-11 08:50:31 +0700

    Menambahkan visibility password untuk register user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,7 +36,7 @@
 
             <div class="mb-4">
                 <label class="block text-sm font-medium text-gray-700 mb-1">Password</label>
-                <input type="password" name="password"
+                <input type="password." name="password"
                     class="w-full border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-green-400">
             </div>
 
